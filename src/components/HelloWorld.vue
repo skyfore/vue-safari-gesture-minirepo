4d9a2769cceb46e01732c476be1689021295acd2
skyfore <skyfore@live.com>
2019-05-06 12:40:03 +0800

    first init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <router-link to="hello">
+      Go to Hello
+    </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/pages/Hello.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="hello">
+    <div>Hello</div>
+    <i>{{notice}}</i>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Hello',
+  data() {
+    return {
+      sure: false,
+      notice: `Can't level`,
+    }
+  },
+  beforeRouteLeave (to, from, next) {
+    if (this.sure) return next();
+    this.sure = true;
+    this.notice = 'Just bye';
+    next(false);
+  }
+}
+</script>
+
+<style>
+.hello {
+  background: yellow;
+}
+</style>
